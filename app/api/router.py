d6010cd2b3756e801a00f81cf384042a1f77ebdf
Tianxuan Sun <ts402@duke.edu>
2025-10-19 10:45:57 -0400

    api(router): import singular modules (group, membership) and register routes

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from .routes import users, courses, availabilities
+from .routes import users, courses, availabilities, group, membership  # <- singular modules
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(courses.router, prefix="/courses", tags=["Courses"])
 api_router.include_router(availabilities.router, prefix="/availabilities", tags=["Availabilities"])
+api_router.include_router(group.router, prefix="/groups", tags=["Groups"])
+api_router.include_router(membership.router, prefix="/memberships", tags=["Memberships"])
