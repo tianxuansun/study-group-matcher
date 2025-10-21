285403c9074e5b0aeb330a9e5bbea0c037f6c9ea
Tianxuan Sun <ts402@duke.edu>
2025-10-21 14:40:37 -0400

    fixed router.py

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .routes import users, courses, availabilities, group, membership, matching  # <- singular modules
+from .routes import users, courses, availabilities, group, membership, matching, enrollment  # <- singular modules
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
@@ -7,4 +7,5 @@ api_router.include_router(courses.router, prefix="/courses", tags=["Courses"])
 api_router.include_router(availabilities.router, prefix="/availabilities", tags=["Availabilities"])
 api_router.include_router(group.router, prefix="/groups", tags=["Groups"])
 api_router.include_router(membership.router, prefix="/memberships", tags=["Memberships"])
+api_router.include_router(enrollment.router, prefix="/enrollments", tags=["Enrollments"])
 api_router.include_router(matching.router, prefix="/matching", tags=["Matching"])
